f7250ed85bf75cd715f06b1e96f71b4fae6f4f7d
abenezer4 <abman166@gmail.com>
2025-11-23 16:04:24 +0300

    Eda for the news headlines

---
+++ scripts/loader.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+def load_news_data(filepath):
+    """
+    Loads the news dataset and converts the date column to datetime objects.
+    
+    Args:
+        filepath (str): Path to the CSV file.
+        
+    Returns:
+        pd.DataFrame: Processed DataFrame.
+    """
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"The file {filepath} was not found.")
+        
+    print(f"Loading data from {filepath}...")
+    df = pd.read_csv(filepath, index_col=0)
+    
+
+   
+        
+    return df
